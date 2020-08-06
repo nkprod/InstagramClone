a3293ad0a71f7fb5793f7819f14ff16782e7c29a
Nurlybek Karshiga <nurlybekkarshiga@gmail.com>
2020-08-06 08:46:39 -0400

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Nulrybek Karshyga on 8/6/20.
+//  Copyright © 2020 Nulrybek Karshyga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
